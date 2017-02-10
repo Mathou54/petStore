cece2ed13a893a01e157eb25dfea7d3abfba257f
Mathou54 <matt54425+github.com@gmail.com>
2017-02-10 11:50:27 +0100

    Enhance detail display.

--- src/main/java/src/main/java/com/colinma/controllers/PetController.java
+++ src/main/java/src/main/java/com/colinma/controllers/PetController.java
@@ -4,6 +4,7 @@ import com.colinma.model.Pet;
 import com.colinma.services.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,5 +31,17 @@ public class PetController {
     @ResponseBody
     public List<Pet> list() {
         return this.petService.get();
+    }
+
+    /**
+     * Get a {@link Pet} by its id.<br />
+     * URL: GET <code>/pet/id</code>
+     *
+     * @return the {@link Pet}.
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Pet get(@PathVariable long id) {
+        return this.petService.get(id);
     }
 }
